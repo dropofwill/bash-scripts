226b376de36f0a044bd8b585130c801a2801002d
Will <whp3652@rit.edu>
2015-02-17 08:45:09 -0500

    Pull api implemented

---
+++ pull_api.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+echo "pull_api.sh $0 $1"
+
+curl "http://gdata.youtube.com/feeds/api/videos?alt=json&q=$1" -o ./latest_results.json
+
+sed "s/$1/bananas/g" latest_results.json > ./bananas.txt
